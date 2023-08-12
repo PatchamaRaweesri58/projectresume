588f61411368cbfd376318ed842162a40e2e95c5
PatchamaRaweesri58 <142074674+PatchamaRaweesri58@users.noreply.github.com>
2023-08-12 13:09:42 +0700

    สร้างresume

---
+++ resume.js
@@ -0,0 +1,12 @@
+/*
+Inspired from the design made by 'Teodora':
+https://www.webdesignerforum.co.uk/files/file/63-free-psd-cv-template/
+https://dribbble.com/shots/1141520-PSD-CV-template?list=searches&offset=17
+
+Dark-wall pattern: https://subtlepatterns.com/dark-wall/
+
+Lato Font: https://www.google.com/fonts/specimen/Lato
+
+We love font icons: http://weloveiconfonts.com/
+
+*/
